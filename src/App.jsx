5d4f9dbf71fcc7573f1eb94ac0973b321d213662
Yasmin17-23 <zyasmins1747@gmail.com>
2024-09-30 15:14:29 +0600

    focus crafy basic file created

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1 className='text-4xl text-red-500'>Focus Crafty</h1>
+     
+    </>
+  )
+}
+
+export default App
