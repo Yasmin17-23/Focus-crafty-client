4ebe1170d5aab08067393b4d4d7c3725da381a9b
Yasmin17-23 <zyasmins1747@gmail.com>
2024-10-02 13:56:21 +0600

    footer section created

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,46 +1,45 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import {  NavLink } from "react-router-dom";
 import { AuthContext } from "../../Providers/AuthProvider";
+import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineClose } from "react-icons/ai";
 
 
 const Navbar = () => {
     const { user, logOut } = useContext(AuthContext);
+    const [open, setOpen] = useState(false);
 
 
     const navLinks = <>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/allCraftItems">All Craft Items</NavLink></li>
-        <li><NavLink to="addCraftItem">Add Craft Item</NavLink></li>
-        <li><NavLink to="myCraftList">My Craft List</NavLink></li>
-        <li><NavLink to="/login">Login</NavLink></li>
-        <li><NavLink to="register">Register</NavLink></li>
+        <li className="mb-2 md:mr-2"><NavLink to="/">Home</NavLink></li>
+        <li  className="mb-2 md:mr-2"><NavLink to="/allCraftItems">All Craft Items</NavLink></li>
+        <li  className="mb-2 md:mr-2"><NavLink to="addCraftItem">Add Craft Item</NavLink></li>
+        <li  className="mb-2 md:mr-2"><NavLink to="myCraftList">My Craft List</NavLink></li>
+        <li  className="mb-2 md:mr-2"><NavLink to="/login">Login</NavLink></li>
+        <li  className="mb-2 md:mr-2"><NavLink to="register">Register</NavLink></li>
 
     </>
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor">
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M4 6h16M4 12h8m-8 6h16" />
-                        </svg>
+                    <div tabIndex={0} role="button" className="btn btn-ghost md:hidden text-black text-xl" 
+                    onClick={() => setOpen(!open)}>
+                     {
+                        open === true ? 
+                        <AiOutlineClose></AiOutlineClose> :
+                        <AiOutlineMenu></AiOutlineMenu>
+                     }
+                    
                     </div>
                     <ul
                         tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        className={`menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2
+                            duration-1000 ${open ? 'top-12' : '-top-60'} shadow absolute`}>
                         {navLinks}
                     </ul>
                 </div>
-                <h4 className="text-xl font-bold">Focus<span className="">Crafty</span></h4>
+                <h4 className="text-xl font-bold text-sky-500">Focus<span className="text-sky-600">Crafty</span></h4>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,10 +1,66 @@
-
+import { BsEmojiHeartEyesFill } from "react-icons/bs";
+import { FaWhatsapp } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div>
-            <h2>Last Footer</h2>
+
+        <div className="footer bg-orange-300 text-white  p-10">
+
+            <div>
+                <h4 className="text-xl font-bold text-sky-500">Focus
+                    <span className="text-sky-600">Crafty</span>
+                </h4>
+                <p className="max-w-sm">Our website based on art & craft product related. We are collect many items of art & craft product
+                    for our users.
+                    <BsEmojiHeartEyesFill className="text-red-600 text-2xl font-bold my-3" />
+                </p>
+                <p className="max-w-2xl mx-auto mt-5 font-bold">
+                    Copyright © {new Date().getFullYear()} - All right reserved by Focus Crafty.
+                </p>
+            </div>
+            <nav>
+                <h6 className="footer-title text-xl font-bold">Company</h6>
+                <a className="link link-hover">Home</a>
+                <a className="link link-hover">Abot</a>
+                <a className="link link-hover">Contact</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title text-xl font-bold">Legal</h6>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+            </nav>
+            <form>
+                <h6 className="footer-title text-xl font-bold">Newsletter</h6>
+                <fieldset className="form-control w-80">
+                    <label className="label">
+                        <span className="label-text text-orange-600">Enter your email address</span>
+                    </label>
+                    <div className="join">
+                        <input
+                            type="text"
+                            placeholder="focusCrafty@gmail.com"
+                            className="input input-bordered join-item text-sm" />
+                        <button className="btn bg-amber-700 outline-none text-white join-item">Subscribe</button>
+                    </div>
+                </fieldset>
+                <div >
+                    <div className="flex justify-between items-center space-x-2 text-xl mt-5">
+                        <FaWhatsapp className="text-[#25D366]" />
+                        <FaFacebook className="text-[#1877F2]" />
+                        <FaYoutube className="text-[#FF0000]" />
+                        <FaLinkedin className="text-[#0077B5]" />
+                    </div>
+
+                </div>
+            </form>
+
         </div>
+
+
     );
 };
 
